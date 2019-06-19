f102d548efdf396c9b60ffec322058f01cffb686
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-19 20:54:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,32 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(arr)# code an argument here
+  arr.each_with_index do |element, index|
+    puts "#{index+1} #{element}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arr)
+  arr.map! {|name| name.capitalize }
+  arr.collect! {|x| x + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+   arr.any? {|x| x.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if arr.include?(cheese_types[0]) == true || arr.include?(cheese_types[1]) == true || arr.include?(cheese_types[2]) == true
+    arr.find do |element|
+      element.include?(cheese_types[0])
+  
+  
 end
+
+[1, 3, 5, 7].find do |num|
+  num.odd?
+end
+  => 1
+
+
+
